802bbd238011e49d67a5fa3c1ac5cca54a70990d
Torchwood-1 <jacobroberts@virginmedia.com>
2024-04-07 17:46:19 +0100

    opt imports

--- src/main/java/tk/jacobcraft/lucifersplugin/Main.java
+++ src/main/java/tk/jacobcraft/lucifersplugin/Main.java
@@ -1,13 +1,7 @@
 package tk.jacobcraft.lucifersplugin;
 
-import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.scoreboard.*;
 import tk.jacobcraft.lucifersplugin.commands.*;
 import org.bukkit.plugin.java.JavaPlugin;
 import tk.jacobcraft.lucifersplugin.events.*;
